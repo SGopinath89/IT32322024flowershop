3f9572db50dc7cdafbc385ea68bb82a5b0104bd8
TharangaDil <147192645+TharangaDil@users.noreply.github.com>
2024-05-06 23:29:52 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,27 +1,54 @@
 
 import './CSS/main.css';
-import Shop from './Shop';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import userim from './img/user.png';
 import cartim from './img/cart.png';
 
 
 export default function Home(){
+    const navigate = useNavigate(); 
+
+    const goToShop = () => {
+        navigate('/shop'); 
+    };
+
+    const goToHome = () =>{
+        navigate('/');
+    };
+
+    const goToAbout=()=>{
+        navigate('/About');
+    };
+
+    const goToContact = () => {
+        navigate('/Contact');
+    };
+
+    const goToUser = () => {
+        navigate('/User');
+    };
+
+    const goToCart = () => {
+        navigate('/Cart');
+    };
+
     return(
         <>
         <nav className='navigation'>
             <ul className='nav-link'>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/Shop'>Shop</a></li>
-                <li><a href='/'>About</a></li>
-                <li><a href='/'>Contact</a></li>
+                <li><a href='/' onClick={goToHome}>Home</a></li>
+                <li><a href='/Shop' onClick={goToShop}>Shop</a></li>
+                <li><a href='/About' onClick={goToAbout}>About</a></li>
+                <li><a href='/Contact' onClick={goToContact}>Contact</a></li>
                 
                 <ul className='root'>
-            <li className='cart'><a href='/'><button  style={{ border: 'none', background: 'none', padding: 0 }}>
-      <img src={cartim} style={{ width: 35, height: 40, cursor: 'pointer' }} />
-    </button> </a></li>
-                <li className='user'><a href='/'><button  style={{ border: 'none', background: 'none', padding: 0 }}>
-      <img src={userim}  style={{ width: 35, height: 40, cursor: 'pointer' }} />
-    </button> </a></li>
+            <li className='cart'><button  style={{ border: 'none', background: 'none', padding: 0 }} onClick={goToCart}>
+                  <img src={cartim} style={{ width: 35, height: 40, cursor: 'pointer' }} />
+            </button> </li>
+                <li className='user'><button  style={{ border: 'none', background: 'none', padding: 0 }} onClick={goToUser}>
+                     <img src={userim}  style={{ width: 35, height: 40, cursor: 'pointer' }} />
+                </button> </li>
             </ul>
             
             </ul>
@@ -33,7 +60,7 @@ export default function Home(){
               <div className='name'>Florist Flowers</div>
            
               <p className='p1'>Nutural & Beautiful Flowers</p>
-              <button className='btn' onClick={Shop}>Shop Now</button>
+              <button className='btn' onClick={goToShop}>Shop Now</button>
             </div>
             
         </>
