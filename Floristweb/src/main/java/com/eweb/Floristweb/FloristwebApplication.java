290551d1a400d2a90fb69f6df755c43b433ac7a0
TharangaDil <2019ict48@vau.jfn.ac.lk>
2024-06-14 12:31:17 +0530

    Start create backend

---
+++ Floristweb/src/main/java/com/eweb/Floristweb/FloristwebApplication.java
@@ -0,0 +1,13 @@
+package com.eweb.Floristweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloristwebApplication.class, args);
+	}
+
+}
---
+++ Floristweb/src/test/java/com/eweb/Floristweb/FloristwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eweb.Floristweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
