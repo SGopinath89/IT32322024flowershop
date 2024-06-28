d4b3f367c924045937b368bef7ef1941f8104967
TharangaDil <2019ict48@vau.jfn.ac.lk>
2024-06-28 11:37:01 +0530

    Edit backend

---
+++ Floristweb/src/main/java/com/eweb/Floristweb/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.eweb.Floristweb.Service;
+
+public interface UserService {
+}
---
+++ Floristweb/src/main/java/com/eweb/Floristweb/Service/UserServiceImp.java
@@ -0,0 +1,9 @@
+package com.eweb.Floristweb.Service;
+
+import com.eweb.Floristweb.Repo.UserRepo;
+
+
+
+public class UserServiceImp {
+
+}
